b58488b4f704304de2c3929989d8c6e1511ec8a9
Adam Bowles <github@adambowl.es>
2023-02-24 19:47:29 +0000

    Add tailwind and KonstaUI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { App, Page, Navbar, Block } from 'konsta/react';
 
-function App() {
+function Main() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <App theme="ios">
+      <Page>
+        <Navbar title="My App" />
 
-export default App;
+        <Block strong>Hello world!</Block>
+      </Page>
+    </App>
+  );
+};
+
+export default Main;
