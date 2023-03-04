5465ef778085c48c897d16ff1db5d930e4d52d89
Adam Bowles <github@adambowl.es>
2023-03-04 17:37:25 +0000

    Prettier

--- src/utils/platform-detector.ts
+++ src/utils/platform-detector.ts
@@ -1,16 +1,17 @@
 const getOperatingSystem = (): 'ios' | 'material' => {
-  if( [
-    'iPad Simulator',
-    'iPhone Simulator',
-    'iPod Simulator',
-    'iPad',
-    'iPhone',
-    'iPod'
-  ].includes(navigator.platform)
-  // iPad on iOS 13 detection
-  || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+  if (
+    [
+      'iPad Simulator',
+      'iPhone Simulator',
+      'iPod Simulator',
+      'iPad',
+      'iPhone',
+      'iPod',
+    ].includes(navigator.platform) ||
+    // iPad on iOS 13 detection
+    (navigator.userAgent.includes('Mac') && 'ontouchend' in document)
   ) {
-    return "ios";
+    return 'ios';
   }
 
   return 'material';
