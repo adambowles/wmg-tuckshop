b2326a6a155baa02990f6f5d095ad940697fa357
Adam Bowles <github@adambowl.es>
2023-03-11 10:21:27 +0000

    Update app name

--- src/components/App/component.test.tsx
+++ src/components/App/component.test.tsx
@@ -5,6 +5,6 @@ import App from 'components/App';
 
 test('renders learn react link', () => {
   // render(<App />);
-  // const linkElement = screen.getByText(/WMG Tuckshop/i);
+  // const linkElement = screen.getByText(/3 AAC WMG Tuckshop/i);
   // expect(linkElement).toBeInTheDocument();
 });
